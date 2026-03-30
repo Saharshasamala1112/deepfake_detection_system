e109c8b704f613b37b26968f4cefd483216de738
Saharshasamala1112 <samalasaharsha23@ifheindia.org>
2026-03-31 05:10:40 +0530

    EfficientNet

--- models/spatial/efficientnet.py
+++ models/spatial/efficientnet.py
@@ -1,11 +1,18 @@
-import torchvision.models as models
+import torch
 import torch.nn as nn
+from torchvision.models import efficientnet_b0, EfficientNet_B0_Weights
 
-class SpatialModel(nn.Module):
+
+class EfficientNetSpatial(nn.Module):
     def __init__(self):
         super().__init__()
-        self.model = models.efficientnet_b4(pretrained=True)
-        self.model.classifier = nn.Identity()
+
+        self.model = efficientnet_b0(weights=EfficientNet_B0_Weights.DEFAULT)
+        in_features = self.model.classifier[1].in_features
+
+        self.model.classifier = nn.Sequential(
+            nn.Linear(in_features, 1)
+        )
 
     def forward(self, x):
         return self.model(x)
