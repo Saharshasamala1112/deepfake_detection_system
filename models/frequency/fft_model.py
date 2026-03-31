2f2acbf9c7b582bdbc1c5c065f78966582d94006
Saharshasamala1112 <samalasaharsha23@ifheindia.org>
2026-03-31 05:37:22 +0530

    FrequencyBranch
    Removed the extract_fft function and added FrequencyBranch class with a neural network model.

--- models/frequency/fft_model.py
+++ models/frequency/fft_model.py
@@ -1,4 +1,25 @@
-import torch.fft as fft
+import torch
+import torch.nn as nn
 
-def extract_fft(x):
-    return fft.fft(x).real
+
+class FrequencyBranch(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Flatten(),
+            nn.Linear(32 * 56 * 56, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x):
+        return self.model(x)
