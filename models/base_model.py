00e73c3a507db20d903c22dd34e8ebcaf3b0060f
Saharshasamala1112 <samalasaharsha23@ifheindia.org>
2026-03-31 15:48:22 +0530

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 import torch
+from torchvision.models import resnet18
 import torch.nn as nn
 
-from models.spatial.efficientnet import EfficientNetSpatial
 from models.frequency.fft_model import FrequencyBranch
 
 
@@ -9,21 +9,8 @@ class DeepfakeDetector(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.spatial = EfficientNetSpatial()
-        self.frequency = FrequencyBranch()
-
-        self.classifier = nn.Sequential(
-            nn.Linear(self.spatial.feature_dim + 32, 128),
-            nn.ReLU(),
-            nn.Dropout(0.3),
-            nn.Linear(128, 1)
-        )
+        self.spatial = resnet18(pretrained=True)
+        self.spatial.fc = nn.Linear(512,1)
 
     def forward(self, x):
-        spatial_feat = self.spatial(x)
-        freq_feat = self.frequency(x)
-
-        combined = torch.cat([spatial_feat, freq_feat], dim=1)
-
-        out = self.classifier(combined)
-        return torch.sigmoid(out)
+        return self.spatial
